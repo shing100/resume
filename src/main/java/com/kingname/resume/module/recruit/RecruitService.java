b8d6a7ebed73469c8ab3eb5a7b0b33f0746902b6
Lim Geun <shing100@naver.com>
2021-05-01 20:43:05 +0900

    resteplate 방법 추가

--- src/main/java/com/kingname/resume/module/recruit/RecruitService.java
+++ src/main/java/com/kingname/resume/module/recruit/RecruitService.java
@@ -2,8 +2,19 @@ package com.kingname.resume.module.recruit;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -12,7 +23,23 @@ public class RecruitService {
 
     private RestTemplate restTemplate;
 
-    public void saveRecuritList() {
-        
+    @Value("${saramin.recruit}")
+    private String SARAMIN_RECRUIT_URL;
+
+    public void saveRecruitList() {
+
+    }
+
+    private Map<String, Object> getUrlResponse(JSONObject parameters) {
+        try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+            HttpEntity<Object> httpEntity = new HttpEntity<>(parameters.toString(), httpHeaders);
+            Map<String, Object> body = restTemplate.exchange(SARAMIN_RECRUIT_URL, HttpMethod.POST, httpEntity, Map.class).getBody();
+            return body;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
