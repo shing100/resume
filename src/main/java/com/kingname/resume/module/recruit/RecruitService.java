f33c1bd67fb32e501ee0ec365ed0fd0ba68666f2
Lim Geun <shing100@naver.com>
2021-05-06 22:55:14 +0900

    사람인 API 수신 테스트 완료

--- src/main/java/com/kingname/resume/module/recruit/RecruitService.java
+++ src/main/java/com/kingname/resume/module/recruit/RecruitService.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
@@ -22,19 +24,18 @@ public class RecruitService {
     private final RecruitProperties recruitProperties;
 
     public void saveRecruitList() {
-        JSONObject parameters = new JSONObject();
-        parameters.put("access-key", "");
-        Map<String, Object> urlResponse = getUrlResponse(parameters);
+        UriComponents build = UriComponentsBuilder.fromHttpUrl(recruitProperties.getRecruit())
+                .queryParam("access-key", recruitProperties.getKey()).build();
+        Map<String, Object> urlResponse = getUrlResponse(build.toUriString());
         log.info(urlResponse.toString());
     }
 
-    private Map<String, Object> getUrlResponse(JSONObject parameters) {
+    private Map<String, Object> getUrlResponse(String url) {
         try {
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
-            HttpEntity<Object> httpEntity = new HttpEntity<>(parameters.toString(), httpHeaders);
-            Map<String, Object> body = restTemplate.exchange(recruitProperties.getRecruit(), HttpMethod.POST, httpEntity, Map.class).getBody();
+            HttpEntity<Object> httpEntity = new HttpEntity<>(httpHeaders);
+            Map<String, Object> body = restTemplate.exchange(url, HttpMethod.GET, httpEntity, Map.class).getBody();
             return body;
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
